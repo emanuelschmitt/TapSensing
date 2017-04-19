8e66fd69cf124bb7b433061c93400188f91ec9a3
Emanuel Schmitt <emanuel.schmitt@me.com>
2017-04-19 17:30:40 +0200

    inital commit

--- BackgroundSensing/ViewController.swift
+++ BackgroundSensing/ViewController.swift
@@ -7,12 +7,70 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    var gyroTimer = Timer()
+    var funcTimer = Timer()
+    
+    var backgroundTask = BackgroundTask()
+    var counter = 0
+    let motionManager = CMMotionManager()
+
+    
+    @IBOutlet weak var startTaskButton: UIButton!
+    @IBOutlet weak var stopTaskButton: UIButton!
+    
+    
+    @IBAction func startBackgroundTask(sender: AnyObject) {
+        backgroundTask.startBackgroundTask()
+        
+        motionManager.startAccelerometerUpdates()
+        motionManager.startGyroUpdates()
+        motionManager.startMagnetometerUpdates()
+        motionManager.startDeviceMotionUpdates()
+        
+        gyroTimer = Timer.scheduledTimer(
+            timeInterval: 0.01,
+            target: self,
+            selector: #selector(self.update),
+            userInfo: nil,
+            repeats: true
+        )
+        
+        
+        funcTimer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(self.timerAction),
+            userInfo: nil,
+            repeats: true
+        )
+        
+        startTaskButton.alpha = 0.5
+        startTaskButton.isUserInteractionEnabled = false
+        
+        stopTaskButton.alpha = 1
+        stopTaskButton.isUserInteractionEnabled = true
+    }
+    
+    @IBAction func stopBackgroundTask(sender: AnyObject) {
+        startTaskButton.alpha = 1
+        startTaskButton.isUserInteractionEnabled = true
+        stopTaskButton.alpha = 0.5
+        stopTaskButton.isUserInteractionEnabled = false
+        
+        funcTimer.invalidate()
+        gyroTimer.invalidate()
+        
+        backgroundTask.stopBackgroundTask()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +78,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func timerAction() {
+        // print("SomeCoolTaskRunning..... \(counter)")
+        counter += 1
+    }
+    
+    func update() {
+        if let accelerometerData = motionManager.accelerometerData {
+            // print(accelerometerData)
+        }
+        if let gyroData = motionManager.gyroData {
+            print(gyroData)
+        }
+        if let magnetometerData = motionManager.magnetometerData {
+            // print(magnetometerData)
+        }
+        if let deviceMotion = motionManager.deviceMotion {
+            // print(deviceMotion)
+        }
+    }
 }
 
