fd80746ddf5bc11c45b5f550424c839c9ce36bab
Emanuel Schmitt <emanuel.schmitt@me.com>
2017-06-21 13:18:20 +0200

    remove unused collection

--- BackgroundSensing/JSONSerializableCollection.swift
+++
@@ -1,7 +0,0 @@
-public struct JSONSerializableCollection: JSONSerializable {
-    public var data: [JSONSerializable]
-    
-    public init(data: [JSONSerializable]){
-        self.data = data
-    }
-}
