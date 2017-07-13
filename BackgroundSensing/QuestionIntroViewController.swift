59ca1c12372487aa39ea008104abb8ac06bc7442
Emanuel Schmitt <emanuel.schmitt@me.com>
2017-07-13 18:16:50 +0200

    added questionintoviewcontroller

---
+++ BackgroundSensing/QuestionIntroViewController.swift
@@ -0,0 +1,19 @@
+//
+//  QuestionIntroViewController.swift
+//  BackgroundSensing
+//
+//  Created by Emanuel Schmitt on 7/13/17.
+//  Copyright © 2017 Emanuel Schmitt. All rights reserved.
+//
+
+import UIKit
+
+class QuestionIntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
